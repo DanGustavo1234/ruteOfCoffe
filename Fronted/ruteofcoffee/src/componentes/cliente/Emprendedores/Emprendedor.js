c70638df41a6fa04fd48030165ac1533bc68e4a4
@DanGustavo1234 <dacordova1234@gmail.com>
2023-01-26 15:23:26 -0500

    commit eprendimientos y emprendedores

--- Fronted/ruteofcoffee/src/componentes/cliente/Emprendedores/Emprendedor.js
+++ Fronted/ruteofcoffee/src/componentes/cliente/Emprendedores/Emprendedor.js
@@ -1,32 +1,66 @@
 import React from 'react'
 import { useEffect, useState } from 'react' 
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavLink} from "react-router-dom";
+import './Emprendedor.scss'
 
 export  function Emprendedor() {
-    const [data,setData]= useState([]);
-    
-    useEffect(()=>{
-        fetch("http://localhost:8000/api/emprendedor/")
-        .then((response)=> response.json())
-        // .then((data)=> console.log(data));
-        .then((data)=> setData(data))
-    })
+    const [data, setData] = useState([]);
 
-  return (
-    <div>
-        [
-            {
-                data.map(
-                (item) => (
-                <div key={item.id}>
-                <h2>{item.nombre}</h2>
-                <p>{item.apellido}</p>
-                </div>
-                )
-                
-                )
-            }
-        ]
-    </div>
-  )
+    useEffect(() => {
+      fetch("http://127.0.0.1:8000/api/emprendedor/")
+        .then((response) => response.json())
+        // .then((data)=> console.log(data));
+        .then((data) => setData(data));
+    });
+    return (
+      <div className="container">
+   
+   <div className="card-group" >
+        
+        {data.map((item) => (
+          <div className="col-md-3">  
+          <div className="card text-center" key={item.id}>
+          
+            <div className="card-body body text-center">
+              <img
+                src={`${item.foto}`}
+                ul={false}
+                className="img_enprendimiento"
+              ></img>
+
+              <NavLink to="/" aria-current="page"><button class="button-38" role="button">{item.first_name}</button></NavLink>
+              
+
+            <div className='row'>
+
+            <ul className='iconos_rs'>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-facebook"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-instagram"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-twitter"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-tiktok"></ion-icon></NavLink>
+          
+            </ul>
+          
+
+            </div>
+          
+           
+             
+            </div>
+            
+           
+  
+          </div> 
+          </div>
+          
+          
+
+        ))}
+        </div>
+      </div>
+  
+     
+      
+    );
 }
--- Fronted/ruteofcoffee/src/componentes/cliente/Emprendimiento/Emprendimiento.js
+++ Fronted/ruteofcoffee/src/componentes/cliente/Emprendimiento/Emprendimiento.js
@@ -1,36 +1,71 @@
 import React from 'react'
 import { useEffect, useState } from 'react' 
+import './Emprendimiento.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { NavLink} from "react-router-dom";
 
 
 
 export function Emprendimiento() {
 
-    const [data,setData]= useState([]);
-    
-    useEffect(()=>{
-        fetch("http://127.0.0.1:8000/api/emprendimiento/")
-        .then((response)=> response.json())
-        // .then((data)=> console.log(data));
-        .then((data)=> setData(data))
-    })
+    const [data, setData] = useState([]);
 
-  return (
-    <div>
-        [
-            {
-                data.map(
-                (item) => (
-                <div key={item.id}>
-                <h2>{item.nombre_emprendimiento}</h2>
-                <p>{item.descripcion}</p>
-                </div>
-                )
-                
-                )
-            }
-        ]
-    </div>
-  )
+    useEffect(() => {
+      fetch("http://127.0.0.1:8000/api/emprendimiento/")
+        .then((response) => response.json())
+        // .then((data)=> console.log(data));
+        .then((data) => setData(data));
+    });
+    return (
+      <div className="container">
+   
+   <div className="card-group" >
+        
+        {data.map((item) => (
+          <div className="col-md-3">  
+          <div className="card text-center" key={item.id}>
+          
+            <div className="card-body body">
+              <img
+                src={`${item.foto}`}
+                ul={false}
+                className="img_enprendimiento"
+              ></img>
+
+              <NavLink to="/" aria-current="page"><button class="button-38" role="button">{item.nombre_emprendimiento}</button></NavLink>
+              
+
+            <div className='row'>
+
+            <ul className='iconos_rs'>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-facebook"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-instagram"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-twitter"></ion-icon></NavLink>
+            <NavLink to="/" aria-current="page"><ion-icon name="logo-tiktok"></ion-icon></NavLink>
+          
+            </ul>
+          
+
+            </div>
+          
+           
+             
+            </div>
+            
+           
+  
+          </div> 
+          </div>
+          
+          
+
+        ))}
+        </div>
+      </div>
+  
+     
+      
+    );
 }
 
 
